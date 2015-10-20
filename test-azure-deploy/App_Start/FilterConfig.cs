e8793b4bd767b8f5fabf9ed4949c18789682a24d
Herry Kurniawan <herry.k@outlook.com>
2015-10-20 18:26:07 +0900

    first commit

---
+++ test-azure-deploy/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace test_azure_deploy
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ test-azure-deploy/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(test_azure_deploy.Startup))]
+namespace test_azure_deploy
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
